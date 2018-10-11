3b906270242b7c0dac94ae3f62311c303680d3bb
jenettewu <43042275+jenettewu@users.noreply.github.com>
2018-10-10 20:45:27 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -40,7 +40,9 @@ def word_is_solved(word, guesses):
     """
     pass
 
-
+    return set(word.replace(' ', '')) <= set(guesses)
+    
+    
 def player_is_dead(wrong_guesses):
     """Determines if the player is dead (too many guesses).
     
